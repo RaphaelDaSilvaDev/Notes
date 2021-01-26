f13687fc9f6ffcf3a9cf1f96a26d2d6eb7c9b079
Rapha <raphael.h.silva@protonmail.com>
2021-01-26 20:58:00 -0300

    update

--- app/src/main/java/com/bits/notes/activities/CreateNoteActivity.kt
+++ app/src/main/java/com/bits/notes/activities/CreateNoteActivity.kt
@@ -1,26 +1,41 @@
 package com.bits.notes.activities
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.TaskInfo
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
+import android.net.Uri
 import android.os.AsyncTask
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.bits.notes.R
 import com.bits.notes.database.NotesDatabase
 import com.bits.notes.entities.Note
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.activity_create_note.*
 import kotlinx.android.synthetic.main.layout_more_slider.*
+import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.jar.Manifest
 
 class CreateNoteActivity : AppCompatActivity() {
     private var selectNoteColor: String = "#303030"
+    private val REQUEST_CODE_STORAGE: Int = 1
+    private val REQUEST_CODE_SELECT_IMAGE: Int = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_note)
@@ -165,10 +180,58 @@ class CreateNoteActivity : AppCompatActivity() {
             image_purple_color.setImageResource(R.drawable.ic_baseline_done)
             setIndicatorColor()
         }
+
+        layoutImage.setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            if(ContextCompat.checkSelfPermission(applicationContext, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_CODE_STORAGE)
+            }else{
+                selectImage()
+            }
+        }
     }
 
     private fun setIndicatorColor(){
         val gradientDrawable = colorIndicator_creteNote.background as GradientDrawable
         gradientDrawable.setColor(Color.parseColor(selectNoteColor))
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun selectImage(){
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        if(intent.resolveActivity(packageManager) != null){
+            startActivityForResult(intent, REQUEST_CODE_SELECT_IMAGE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == REQUEST_CODE_STORAGE && grantResults.size > 0){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                selectImage()
+            }else{
+                Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CODE_SELECT_IMAGE && resultCode == Activity.RESULT_OK){
+            if(data != null){
+                val selectedImageUri: Uri? = data.data
+                if(selectedImageUri != null){
+                    try {
+                        val inputStream = contentResolver.openInputStream(selectedImageUri)
+                        val bitmap = BitmapFactory.decodeStream(inputStream)
+                        imageNote.setImageBitmap(bitmap)
+                    }catch (exception: Exception){
+                        Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 }
